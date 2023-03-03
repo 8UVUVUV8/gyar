9ddfc211636014617f4020acc661b2a055308d01
8UVUVUV8 <sz.andras2400@gmail.com>
2023-03-03 08:14:58 +0100

    minden fent

--- vinyl-ftp.js
+++ vinyl-ftp.js
@@ -4,11 +4,11 @@ var ftp = require( 'vinyl-ftp' );
 var conn = new ftp( {
     host: 'ftpupload.net',
     user: 'epiz_33705804',
-    password: 'vQzSpQGdnBDjk',
+    password: '...............',
     parallel: 10,
     secure: true,
     secureOptions: {rejectUnauthorized: false}
 });
  
 fs.src( [ './dist/**' ], { buffer: false } )
-    .pipe( conn.dest( '/htdocs/gyar' ) );
+    .pipe( conn.dest( '/htdocs' ) );
